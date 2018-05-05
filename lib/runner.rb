1f5d2bd47bc37802b2b1c157bbc989418333e000
Seth Schwartz <sschwartz214@gmail.com>
2018-05-05 15:34:16 -0600

    adds user interface

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,16 +1,7 @@
 require './lib/credit_check'
 
-cc_number_1 = "4929735477250543"
-cc_number_2 = "5541801923795240"
-cc_number_3 = "342804633855673"
-cc_number_4 = "342801633855673"
+puts "Welcome to credit check.  To see if your credit card is valid please enter the number: "
 
-credit_check_1 = CreditCheck.new(cc_number_1)
-credit_check_2 = CreditCheck.new(cc_number_2)
-credit_check_3 = CreditCheck.new(cc_number_3)
-credit_check_4 = CreditCheck.new(cc_number_4)
-
-credit_check_1.check_validity
-credit_check_2.check_validity
-credit_check_3.check_validity
-credit_check_4.check_validity
+cc_number = gets.chomp
+credit_check = CreditCheck.new(cc_number)
+credit_check.check_validity
