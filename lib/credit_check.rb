eda4adfbb551d9b09508bb5774349c6c7be26984
Seth Schwartz <sschwartz214@gmail.com>
2018-05-06 13:11:55 -0600

    deleted comments

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,46 +1,26 @@
 class CreditCheck
 
-  #initialize with a cc number
   def initialize(cc_number)
     @cc_number = cc_number
   end
 
-  # Perform math on Credit card
   def process_string
-  #Convert number into an array of intergers
      @cc_number.to_i.digits
   end
 
   def double_every_other
-    #Double every other number(every odd index)
-  process_string.map.with_index do |number, index|
-    index.odd? ? number * 2 : number
-  end
-
-    # if index.odd?
-    #   number *= 2
-    # end
-    #   number
-    # end
+    process_string.map.with_index do |number, index|
+      index.odd? ? number * 2 : number
+    end
   end
 
   def subtract_nine
-  # if number > 10, add digits together
     double_every_other.map do |number|
       number > 9 ? number -= 9 : number
-
-      # if number > 9
-      #   number -= 9
-      # end
-      #   number
     end
   end
 
   def check_validity
-    # sum of all numbers
-    # Divide by 10
-  # check the result against the accepted answer
-  # Print whether the card is valid or not
     if subtract_nine.sum % 10 == 0
       p "The number is valid!"
     else
